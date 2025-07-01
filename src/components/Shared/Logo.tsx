d4083f596e904e8d5a757058fb5fe14281dcebda
RecepIlcan <recep.ilcan@tss-x.com>
2025-07-01 21:18:32 +0300

    fix: logo image not loading correctly on page refresh due to relative path
    +Changed logo src to use an absolute path from the public folder to ensure consistent loading across all routes, especially after hard refreshes.

--- src/components/Shared/Logo.tsx
+++ src/components/Shared/Logo.tsx
@@ -8,7 +8,7 @@ const Logo = ({ onClick }: LogoProps) => {
   return (
     <RouterLink to="/" className="logo" onClick={onClick}>
       <img
-        src="img/tssx/tssx-light-logo.png"
+        src="/img/tssx/tssx-light-logo.png"
         alt="TSS-X Logo"
       />
     </RouterLink>
